e348716164e56715d973deaa23d2e639804c3624
abdllhcay <seyyahleo@yandex.com>
2019-02-27 08:06:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -83,66 +83,6 @@ int _tmain(int argc, _TCHAR* argv[])
 	cout << "\033[0;32m[+]\033[0m Number of objects: " << count << endl;
 	cout << "\033[0;32m[+]\033[0m Number of chickpeas: " << numChickpea << endl;
 	cout << "\033[0;32m[+]\033[0m Number of lentils: " << numLentil << endl << endl;
-	
-	// Check results if required
-	/*BYTE* display_imge = 0;
-	int choice = 1;
-	while (choice != 0){
-		cout << "1. Convert to binary" << endl;
-		cout << "2. Boundary extraction" << endl;
-		cout << "3. Erosion" << endl;
-		cout << "4. Dilation" << endl;
-		cout << "5. Colored CCA" << endl;
-		cout << "0. Exit the program" << endl;
-		cout << "> ";
-		cin >>  choice;
-		cout << endl;
-
-		switch (choice){
-		case 1:
-			display_imge = ConvertIntensityToBMP(bin, Width, Height, &new_size);
-			output = L"1.bmp";
-			printf("\033c");
-			if (SaveBMP(display_imge, Width, Height, new_size, output))
-				cout << "\033[0;32m[+]\033[0m Output Image was successfully saved" << endl << endl;
-			else cout << "\033[1;31m[-]\033[0m Error on saving image" << endl;
-			break;
-		case 2:
-			display_imge = ConvertIntensityToBMP(be, Width, Height, &new_size);
-			output = L"2.bmp";
-			printf("\033c");
-			if (SaveBMP(display_imge, Width, Height, new_size, output))
-				cout << "\033[0;32m[+]\033[0m Output Image was successfully saved" << endl << endl;
-			else cout << "\033[1;31m[-]\033[0m Error on saving image" << endl;
-			break;
-		case 3:
-			display_imge = ConvertIntensityToBMP(er, Width, Height, &new_size);
-			output = L"3.bmp";
-			printf("\033c");
-			if (SaveBMP(display_imge, Width, Height, new_size, output))
-				cout << "\033[0;32m[+]\033[0m Output Image was successfully saved" << endl << endl;
-			else cout << "\033[1;31m[-]\033[0m Error on saving image" << endl;
-			break;
-		case 4:
-			display_imge = ConvertIntensityToBMP(dil, Width, Height, &new_size);
-			output = L"4.bmp";
-			printf("\033c");
-			if (SaveBMP(display_imge, Width, Height, new_size, output))
-				cout << "\033[0;32m[+]\033[0m Output Image was successfully saved" << endl << endl;
-			else cout << "\033[1;31m[-]\033[0m Error on saving image" << endl;
-			break;
-		case 5:
-			display_imge = ConvertIntensityToBMP(col, Width, Height, &new_size);
-			output = L"5.bmp";
-			printf("\033c");
-			if (SaveBMP(display_imge, Width, Height, new_size, output))
-				cout << "\033[0;32m[+]\033[0m Output Image was successfully saved" << endl << endl;
-			else cout << "\033[1;31m[-]\033[0m Error on saving image" << endl;
-			break;
-		case 0:
-			cout << "\033[1;34m[?]\033[0m Exiting program. See ya!" << endl;
-		}
-	}*/
 
 	delete[] display_imge;
 	delete[] temp_tag;
